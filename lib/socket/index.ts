aebef1238fef722b4079524e71cf97e525beb126
Matthew Marshall <mamarshall44@gmail.com>
2026-04-01 15:41:55 -0700

    fixed websockets

--- lib/socket/index.ts
+++ lib/socket/index.ts
@@ -19,4 +19,5 @@ const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 --------------------------------------------------------------------------- */
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
+  transports: ['websocket'],
 });
